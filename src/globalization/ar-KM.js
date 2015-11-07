938608a85c2893b106b7313cedb0e2b95c7fe74f
alqfahad <alqfahad@gmail.com>
2015-11-07 15:28:34 +0300

    minor fix

--- src/globalization/ar-KM.js
+++ src/globalization/ar-KM.js
@@ -1,5 +1,5 @@
 Date.CultureInfo = {
-	/* Culture Name */
+    /* Culture Name */
     name: "ar-KM",
     englishName: "Arabic (Comoros)",
     nativeName: "العربية (جزر القمر)",
@@ -14,7 +14,7 @@ Date.CultureInfo = {
     monthNames: ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"],
     iatedMonthNames: ["ينا", "فبر", "مار", "أبر", "ماي", "يون", "يول", "أغس", "سبت", "أكت", "نوف", "ديس"],
 
-	/* AM/PM Designators */
+    /* AM/PM Designators */
     amDesignator: "ص",
     pmDesignator: "م",
 
@@ -124,10 +124,9 @@ Date.CultureInfo = {
 		
 	shortMeridian: /^(ص|م)/i,
 	longMeridian: /^(صباح(ا)?|مساء(ا)?)/i,
-  timezone: /^(((gmt)?\s*(\+|\-)\s*\d\d\d\d?)|gmt|utc|((ب(ال)?)?توقيت )?((جمهورية )?السودان|الخرطوم|شرق افريقيا|eat|eastern africa? tim(e|ing)))/i,
-        
-  ordinalSuffix: /^\s*(st|nd|rd|th)/i,
-  timeContext: /^\s*(\:|a(?!u|p)|p)/i
+	timezone: /^(((gmt)?\s*(\+|\-)\s*\d\d\d\d?)|gmt|utc|((ب(ال)?)?توقيت )?((جمهورية )?السودان|الخرطوم|شرق افريقيا|eat|eastern africa? tim(e|ing)))/i,
+        ordinalSuffix: /^\s*(st|nd|rd|th)/i,
+  	timeContext: /^\s*(\:|a(?!u|p)|p)/i
     },
 
 	timezones: [{name:"UTC", offset:"+000"}, {name:"GMT", offset:"+000"}, {name:"EAT", offset:"+0300"}, {name:"AST", offset:"+0300"}]
