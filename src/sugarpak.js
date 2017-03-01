c3f6e7ccc0da25e3afda594dadebbee6466a8da1
sibu <sibStephen@users.noreply.github.com>
2017-03-02 00:25:29 +0530

    Changes in usage of this as a reference to using it  as a variable and passing it. (#84)

--- src/sugarpak.js
+++ src/sugarpak.js
@@ -12,7 +12,7 @@
  ** SugarPak - Domain Specific Language -  Syntactical Sugar **
  **************************************************************
  */
- 
+
 (function () {
     var $D = Date, $P = $D.prototype, $C = $D.CultureInfo, $N = Number.prototype;
 
@@ -27,14 +27,14 @@
 
     // private
     $P._same = false;
-    
+
     // private
     $P._isSecond = false;
 
     // private
     $N._dateElement = "day";
 
-    /** 
+    /**
      * Moves the date to the next instance of a date as specified by the subsequent date element function (eg. .day(), .month()), month name function (eg. .january(), .jan()) or day name function (eg. .friday(), fri()).
      * Example
     <pre><code>
@@ -44,7 +44,7 @@
     Date.today().next().mar();
     Date.today().next().week();
     </code></pre>
-     * 
+     *
      * @return {Date}    date
      */
     $P.next = function () {
@@ -52,7 +52,7 @@
         return this;
     };
 
-    /** 
+    /**
      * Creates a new Date (Date.today()) and moves the date to the next instance of the date as specified by the subsequent date element function (eg. .day(), .month()), month name function (eg. .january(), .jan()) or day name function (eg. .friday(), fri()).
      * Example
     <pre><code>
@@ -62,14 +62,14 @@
     Date.next().mar();
     Date.next().week();
     </code></pre>
-     * 
+     *
      * @return {Date}    date
-     */    
+     */
     $D.next = function () {
         return $D.today().next();
     };
 
-    /** 
+    /**
      * Moves the date to the previous instance of a date as specified by the subsequent date element function (eg. .day(), .month()), month name function (eg. .january(), .jan()) or day name function (eg. .friday(), fri()).
      * Example
     <pre><code>
@@ -79,15 +79,16 @@
     Date.today().last().mar();
     Date.today().last().week();
     </code></pre>
-     *  
+     *
      * @return {Date}    date
      */
     $P.last = $P.prev = $P.previous = function () {
-        this._orient = -1;
-        return this;
+        var previous = this;
+        previous._orient = -1;
+        return previous;
     };
 
-    /** 
+    /**
      * Creates a new Date (Date.today()) and moves the date to the previous instance of the date as specified by the subsequent date element function (eg. .day(), .month()), month name function (eg. .january(), .jan()) or day name function (eg. .friday(), fri()).
      * Example
     <pre><code>
@@ -97,14 +98,14 @@
     Date.prev().mar();
     Date.last().week();
     </code></pre>
-     *  
+     *
      * @return {Date}    date
      */
     $D.last = $D.prev = $D.previous = function () {
         return $D.today().last();
-    };    
+    };
 
-    /** 
+    /**
      * Performs a equality check when followed by either a month name, day name or .weekday() function.
      * Example
     <pre><code>
@@ -113,19 +114,19 @@
     Date.today().is().march();
     Date.today().is().mar();
     </code></pre>
-     *  
+     *
      * @return {Boolean}    true|false
      */
-    $P.is = function () { 
-        this._is = true; 
-        return this; 
+    $P.is = function () {
+        this._is = true;
+        return this;
     };
 
-    /** 
+    /**
      * Determines if two date objects occur on/in exactly the same instance of the subsequent date part function.
      * The function .same() must be followed by a date part function (example: .day(), .month(), .year(), etc).
      *
-     * An optional Date can be passed in the date part function. If now date is passed as a parameter, 'Now' is used. 
+     * An optional Date can be passed in the date part function. If now date is passed as a parameter, 'Now' is used.
      *
      * The following example demonstrates how to determine if two dates fall on the exact same day.
      *
@@ -136,33 +137,33 @@
 
     // Do they occur on the same day?
     d1.same().day(d2); // true
-    
+
      // Do they occur on the same hour?
     d1.same().hour(d2); // false, unless d2 hour is '00' (midnight).
-    
+
     // What if it's the same day, but one year apart?
     var nextYear = Date.today().add(1).year();
 
-    d1.same().day(nextYear); // false, because the dates must occur on the exact same day. 
+    d1.same().day(nextYear); // false, because the dates must occur on the exact same day.
     </code></pre>
      *
-     * Scenario: Determine if a given date occurs during some week period 2 months from now. 
+     * Scenario: Determine if a given date occurs during some week period 2 months from now.
      *
      * Example
     <pre><code>
     var future = Date.today().add(2).months();
     return someDate.same().week(future); // true|false;
     </code></pre>
-     *  
+     *
      * @return {Boolean}    true|false
-     */    
-    $P.same = function () { 
+     */
+    $P.same = function () {
         this._same = true;
         this._isSecond = false;
-        return this; 
+        return this;
     };
 
-    /** 
+    /**
      * Determines if the current date/time occurs during Today. Must be preceded by the .is() function.
      * Example
     <pre><code>
@@ -171,31 +172,31 @@
     Date.today().is().today();// true
     Date.today().add(-1).day().is().today(); // false
     </code></pre>
-     *  
+     *
      * @return {Boolean}    true|false
-     */    
+     */
     $P.today = function () {
         return this.same().day();
     };
 
-    /** 
+    /**
      * Determines if the current date is a weekday. This function must be preceded by the .is() function.
      * Example
     <pre><code>
     Date.today().is().weekday(); // true|false
     </code></pre>
-     *  
+     *
      * @return {Boolean}    true|false
      */
     $P.weekday = function () {
-        if (this._is) { 
+        if (this._is) {
             this._is = false;
             return (!this.is().sat() && !this.is().sun());
         }
         return false;
     };
 
-    /** 
+    /**
      * Sets the Time of the current Date instance. A string "6:15 pm" or config object {hour:18, minute:15} are accepted.
      * Example
     <pre><code>
@@ -205,14 +206,14 @@
     // Set time to 6:15pm with a config object
     Date.today().at({hour:18, minute:15});
     </code></pre>
-     *  
+     *
      * @return {Date}    date
      */
     $P.at = function (time) {
         return (typeof time === "string") ? $D.parse(this.toString("d") + " " + time) : this.set(time);
-    }; 
-        
-    /** 
+    };
+
+    /**
      * Creates a new Date() and adds this (Number) to the date based on the preceding date element function (eg. second|minute|hour|day|month|year).
      * Example
     <pre><code>
@@ -220,11 +221,11 @@
     (3).days().fromNow();
     (6).months().fromNow();
 
-    // Declared Number variables do not require parentheses. 
+    // Declared Number variables do not require parentheses.
     var n = 6;
     n.months().fromNow();
     </code></pre>
-     *  
+     *
      * @return {Date}    A new Date instance
      */
     $N.fromNow = $N.after = function (date) {
@@ -233,7 +234,7 @@
         return ((!date) ? new Date() : date.clone()).add(c);
     };
 
-    /** 
+    /**
      * Creates a new Date() and subtract this (Number) from the date based on the preceding date element function (eg. second|minute|hour|day|month|year).
      * Example
     <pre><code>
@@ -241,11 +242,11 @@
     (3).days().ago();
     (6).months().ago();
 
-    // Declared Number variables do not require parentheses. 
+    // Declared Number variables do not require parentheses.
     var n = 6;
     n.months().ago();
     </code></pre>
-     *  
+     *
      * @return {Date}    A new Date instance
      */
     $N.ago = $N.before = function (date) {
@@ -260,23 +261,23 @@
         mx = ("january february march april may june july august september october november december").split(/\s/),
         px = ("Millisecond Second Minute Hour Day Week Month Year").split(/\s/),
         pxf = ("Milliseconds Seconds Minutes Hours Date Week Month FullYear").split(/\s/),
-		nth = ("final first second third fourth fifth").split(/\s/),
+        nth = ("final first second third fourth fifth").split(/\s/),
         de;
 
-   /** 
+   /**
      * Returns an object literal of all the date parts.
      * Example
     <pre><code>
-	var o = new Date().toObject();
-	
-	// { year: 2008, month: 4, week: 20, day: 13, hour: 18, minute: 9, second: 32, millisecond: 812 }
-	
-	// The object properties can be referenced directly from the object.
-	
-	alert(o.day);  // alerts "13"
-	alert(o.year); // alerts "2008"
+    var o = new Date().toObject();
+
+    // { year: 2008, month: 4, week: 20, day: 13, hour: 18, minute: 9, second: 32, millisecond: 812 }
+
+    // The object properties can be referenced directly from the object.
+
+    alert(o.day);  // alerts "13"
+    alert(o.year); // alerts "2008"
     </code></pre>
-     *  
+     *
      * @return {Date}    An object literal representing the original date object.
      */
     $P.toObject = function () {
@@ -285,66 +286,67 @@
             o[px[i].toLowerCase()] = this["get" + pxf[i]]();
         }
         return o;
-    }; 
-   
-   /** 
-     * Returns a date created from an object literal. Ignores the .week property if set in the config. 
+    };
+
+   /**
+     * Returns a date created from an object literal. Ignores the .week property if set in the config.
      * Example
     <pre><code>
-	var o = new Date().toObject();
-	
-	return Date.fromObject(o); // will return the same date. 
+    var o = new Date().toObject();
+
+    return Date.fromObject(o); // will return the same date.
 
     var o2 = {month: 1, day: 20, hour: 18}; // birthday party!
     Date.fromObject(o2);
     </code></pre>
-     *  
+     *
      * @return {Date}    An object literal representing the original date object.
-     */    
+     */
     $D.fromObject = function(config) {
         config.week = null;
         return Date.today().set(config);
     };
-        
+
     // Create day name functions and abbreviated day name functions (eg. monday(), friday(), fri()).
     var df = function (n) {
-        return function () { 
-            if (this._is) { 
-                this._is = false; 
-                return this.getDay() == n; 
+        return function () {
+            var dayName = this;
+            if (dayName._is) {
+                dayName._is = false;
+                return dayName.getDay() == n;
             }
-            if (this._nth !== null) {
-                // If the .second() function was called earlier, remove the _orient 
+            if (dayName._nth !== null) {
+                // If the .second() function was called earlier, remove the _orient
                 // from the date, and then continue.
                 // This is required because 'second' can be used in two different context.
-                // 
+                //
                 // Example
                 //
                 //   Date.today().add(1).second();
                 //   Date.march().second().monday();
-                // 
+                //
                 // Things get crazy with the following...
                 //   Date.march().add(1).second().second().monday(); // but it works!!
-                //  
-                if (this._isSecond) {
-                    this.addSeconds(this._orient * -1);
+                //
+                if (dayName._isSecond) {
+                    dayName.addSeconds(dayname._orient * -1);
                 }
                 // make sure we reset _isSecond
-                this._isSecond = false;
+                dayName._isSecond = false;
 
-                var ntemp = this._nth;
-                this._nth = null;
-                var temp = this.clone().moveToLastDayOfMonth();
-                this.moveToNthOccurrence(n, ntemp);
-                if (this > temp) {
+                var ntemp = dayName._nth;
+                dayName._nth = null;
+                var temp = dayName.clone().moveToLastDayOfMonth();
+                dayName.moveToNthOccurrence(n, ntemp);
+                if (dayName > temp) {
                     throw new RangeError($D.getDayName(n) + " does not occur " + ntemp + " times in the month of " + $D.getMonthName(temp.getMonth()) + " " + temp.getFullYear() + ".");
                 }
-                return this;
-            }			
-            return this.moveToDayOfWeek(n, this._orient);
+                return dayName;
+            }
+            return dayName.moveToDayOfWeek(n, dayName._orient);
         };
     };
-    
+
     var sdf = function (n) {
         return function () {
             var t = $D.today(), shift = n - t.getDay();
@@ -354,8 +356,13 @@
             return t.addDays(shift);
         };
     };
-	
-    for (var i = 0; i < dx.length; i++) {
+
+    //Declaring All the variables used in the code globally.
+     var i ,
+         j ,
+         k ,
+         l;
+    for (i = 0; i < dx.length; i++) {
         // Create constant static Day Name variables. Example: Date.MONDAY or Date.MON
         $D[dx[i].toUpperCase()] = $D[dx[i].toUpperCase().substring(0, 3)] = i;
 
@@ -365,25 +372,26 @@
         // Create Day Name instance functions. Example: Date.today().next().monday()
         $P[dx[i]] = $P[dx[i].substring(0, 3)] = df(i);
     }
-    
+
     // Create month name functions and abbreviated month name functions (eg. january(), march(), mar()).
-    var mf = function (n) { 
+    var mf = function (n) {
         return function () {
-            if (this._is) { 
-                this._is = false; 
-                return this.getMonth() === n; 
+            var monthName = this;
+            if (monthName._is) {
+                monthname._is = false;
+                return monthName.getMonth() === n;
             }
-            return this.moveToMonth(n, this._orient); 
+            return monthName.moveToMonth(n, monthname._orient);
         };
     };
-    
+
     var smf = function (n) {
         return function () {
             return $D.today().set({ month: n, day: 1 });
         };
     };
-    
-    for (var j = 0; j < mx.length; j++) {
+
+    for (j = 0; j < mx.length; j++) {
         // Create constant static Month Name variables. Example: Date.MARCH or Date.MAR
         $D[mx[j].toUpperCase()] = $D[mx[j].toUpperCase().substring(0, 3)] = j;
 
@@ -393,24 +401,25 @@
         // Create Month Name instance functions. Example: Date.today().next().march()
         $P[mx[j]] = $P[mx[j].substring(0, 3)] = mf(j);
     }
-    
+
     // Create date element functions and plural date element functions used with Date (eg. day(), days(), months()).
     var ef = function (j) {
         return function () {
-            // if the .second() function was called earlier, the _orient 
+            var dateElement = this;
+            // if the .second() function was called earlier, the _orient
             // has alread been added. Just return this and reset _isSecond.
-            if (this._isSecond) {
-                this._isSecond = false;
-                return this;
+            if (dateElement._isSecond) {
+                dateElement._isSecond = false;
+                return dateElement;
             }
 
-            if (this._same) {
-                this._same = this._is = false; 
-                var o1 = this.toObject(),
+            if (dateElement._same) {
+                dateElement._same = dateElement._is = false;
+                var o1 = dateElement.toObject(),
                     o2 = (arguments[0] || new Date()).toObject(),
                     v = "",
                     k = j.toLowerCase();
-                    
+
                 for (var m = (px.length - 1); m > -1; m--) {
                     v = px[m].toLowerCase();
                     if (o1[v] != o2[v]) {
@@ -422,43 +431,45 @@
                 }
                 return true;
             }
-            
+
             if (j.substring(j.length - 1) != "s") {
-                j += "s"; 
+                j += "s";
             }
-            return this["add" + j](this._orient);
+            return dateElement["add" + j](dateElement._orient);
         };
     };
-    
-    
+
+
     var nf = function (n) {
         return function () {
-            this._dateElement = n;
-            return this;
+            var dateElement = this;
+            dateElement._dateElement = n;
+            return dateElement;
         };
     };
-   
-    for (var k = 0; k < px.length; k++) {
+
+    for (k = 0; k < px.length; k++) {
         de = px[k].toLowerCase();
-    
+
         // Create date element functions and plural date element functions used with Date (eg. day(), days(), months()).
         $P[de] = $P[de + "s"] = ef(px[k]);
-        
+
         // Create date element functions and plural date element functions used with Number (eg. day(), days(), months()).
         $N[de] = $N[de + "s"] = nf(de);
     }
-    
+
     $P._ss = ef("Second");
-	
+
     var nthfn = function (n) {
         return function (dayOfWeek) {
-            if (this._same) {
-                return this._ss(arguments[0]);
+            var secondDayOfWeek = this;
+            if (secondDayOfWeek._same) {
+                return secondDayOfWeek._ss(arguments[0]);
             }
             if (dayOfWeek || dayOfWeek === 0) {
-                return this.moveToNthOccurrence(dayOfWeek, n);
+                return secondDayOfWeek.moveToNthOccurrence(dayOfWeek, n);
             }
-            this._nth = n;
+            secondDayOfWeek._nth = n;
 
             // if the operator is 'second' add the _orient, then deal with it later...
             if (n === 2 && (dayOfWeek === undefined || dayOfWeek === null)) {
@@ -469,7 +480,7 @@
         };
     };
 
-    for (var l = 0; l < nth.length; l++) {
+    for (l = 0; l < nth.length; l++) {
         $P[nth[l]] = (l === 0) ? nthfn(-1) : nthfn(l);
     }
 }());
